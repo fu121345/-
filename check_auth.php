f0dee7c6e8bac9ead90b8833f157207f5014626d
fu121345 <a0970767592@gmail.com>
2026-04-04 22:52:35 +0800

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -5,15 +5,16 @@ require_once('db_config.php');
 $userid = $_POST['username'];
 $userpwd = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username = '$userid' AND password = '$userpwd'";
-$result = mysqli_query($conn, $query);
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+$stmt->bind_param("s", $userid);
+$stmt->execute();
+$result = $stmt->get_result();
 
-if (mysqli_num_rows($result) > 0) {
-    // ③
-    $_SESSION['username'] = $userid;
-    // ④
-    header("Location: index.php");
-    exit();
+if ($row = $result->fetch_assoc()) {
+    if (password_verify($userpwd, $row['password'])) {
+        $_SESSION['username'] = $userid;
+        header("Location: index.php");
+        exit();
 } else {
     echo "<script>alert('帳號或密碼錯誤'); location.href='login.php';</script>";
 }
