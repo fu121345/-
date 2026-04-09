d70783c81ee9ad506ec72fb1e17b954032733a09
fu121345 <a0970767592@gmail.com>
2026-04-09 21:01:38 +0800

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -2,6 +2,10 @@
 session_start(); // 啟動 Session
 require_once('db_config.php');
 
+if (!isset($_POST['username'], $_POST['password'])) {
+    exit('?沒輸密碼就要進來我操你媽');
+}
+
 $userid = $_POST['username'];
 $userpwd = $_POST['password'];
 
@@ -15,7 +19,7 @@ if ($row = $result->fetch_assoc()) {
         $_SESSION['username'] = $userid;
         header("Location: index.php");
         exit();
-} else {
-    echo "<script>alert('帳號或密碼錯誤'); location.href='login.php';</script>";
+    }
 }
+echo "<script>alert('帳號或密碼錯誤'); location.href='login.php';</script>";
 ?>
